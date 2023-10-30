3092772229d53f4fe9d0ccb5720ee75a83ae3365
Tkachenko01001 <x380999660502@gmail.com>
2023-10-30 17:46:35 +0200

    init/layout

---
+++ src/app/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "node_modules/next/link"
+
+const Header = () => {
+    return (
+        <header>
+            <Link href="publicContract"> Публічний договір </Link>
+        </header>
+    )
+}
+
+export default Header
