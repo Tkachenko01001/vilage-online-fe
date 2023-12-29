552ba215adce5c490399a7f2ab9c781b14c28e0a
Tkachenko01001 <x380999660502@gmail.com>
2023-12-29 04:31:38 +0200

    add/hover_animation_for_button

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -7,15 +7,26 @@ import styles from './hero.module.scss';
 import Track from '../track/Track';
 
 const Hero = () => {
+
+  const handleHover = (e) => {
+        const x = e.pageX - e.target.offsetLeft;
+        const y = e.pageY - e.target.offsetTop;
+
+        e.target.style.setProperty("--x", `${x}px`);
+        e.target.style.setProperty("--y", `${y}px`);
+    };
+
     return (
         <section className={styles.sectionWrapp}>
             <motion.div className={styles.titleWrapp} {...titleAnimation}>
                 <h1 className={styles.title}>Покупки з доставкою до вашого дому</h1>
                 <p className={styles.paragraph}>Зекономимо Ваш час та кошти та забезпечимо доступ до усіх необхідних товарів</p>
-                <button className={styles.btn} type="button">
+                <div className={styles.btnBorder}>
+                <button className={styles.btn} type="button" onMouseMove={handleHover}>
                     <span className={styles.btnText}>Спробувати безкоштовно</span>
                     <Arrow className={styles.iconArow} />
-                </button>
+                    </button>
+                </div>
             </motion.div>
             <div className={`${styles.blurBall} ${styles.blurBallHeight}`}></div>
                 <Elipse className={styles.iconElipse} />
